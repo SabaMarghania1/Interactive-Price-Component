3f26b63e42dcc5571a1f578075360c4f1ba883cd
Saba Marghania <marghanias@gmail.com>
2024-02-10 15:57:54 +0400

    finish switch

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// const slider = document.getElementById("mySlider");
+const slider = document.getElementById("mySlider");
 
-// slider.addEventListener("change", e => {
-//   const value = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
+slider.addEventListener("change", e => {
+  const value = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
 
-//   slider.style.background =
-//     "linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) " +
-//     value +
-//     "%, #ddd " +
-//     value +
-//     "%, #ddd 100%)";
-// });
+  slider.style.background =
+    "linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) " +
+    value +
+    "%, #ddd " +
+    value +
+    "%, #ddd 100%)";
+});
