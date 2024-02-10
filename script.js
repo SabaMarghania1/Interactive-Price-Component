0838b6dd36735e1b4b474763c77844427d0b1823
Saba Marghania <marghanias@gmail.com>
2024-02-10 14:39:23 +0400

    finish: header mobile

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-document.getElementById("mySlider").oninput = function () {
-  var value = ((this.value - this.min) / (this.max - this.min)) * 100;
-  this.style.background =
-    "linear-gradient(to right, #4CAF50 0%, #4CAF50 " + value + "%, #ddd " + value + "%, #ddd 100%)";
-};
+// const slider = document.getElementById("mySlider");
+
+// slider.addEventListener("change", e => {
+//   const value = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
+
+//   slider.style.background =
+//     "linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) " +
+//     value +
+//     "%, #ddd " +
+//     value +
+//     "%, #ddd 100%)";
+// });
